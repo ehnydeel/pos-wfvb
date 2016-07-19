d823558391c428895616aea56ea355998dc960bf
Andreas Kleinbub <andreas.kleinbub@gmail.com>
2016-07-19 15:44:07 +0200

    Add pdf-file for printing. Ajust print-route with correct printing files.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set('view engine', 'ejs');
 // Middleware
 // =======================================
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -74,4 +74,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// =======================================
+// Export app module
+// =======================================
 module.exports = app;
--- routes/print.js
+++ routes/print.js
@@ -10,23 +10,23 @@ var router = express.Router();
 // Configuration 
 // =======================================
 // Avaiable printers: gk420, hp521dw
-var printer = 'hp521dw';
+var printer = '720nw';
 
 // =======================================
 // PRINT LABEL 
 // =======================================
 function printLabel(file) {
-	//var exec = require('child_process').exec;
-	//var command = 'lp -d ' + printer + ' /home/pi/pos/prtsrc/zebra/' + file;
+	var exec = require('child_process').exec;
+	var command = 'lp -d ' + printer + ' /home/pi/pos/prtsrc/brother/' + file + ' -o landscape';
 	
-	//exec(command, function(error, stdout, stderr) {
-	//	if (error !== null) {
-	//		console.log('exec error: ' + error);
-	//	} else {
-	//		console.log('printing /home/pi/pos/prtsrc/' + file);
-	//	}
-	//});
-	console.log('lp -d ' + printer + ' /home/pi/pos/prtsrc/zebra/' + file);
+	exec(command, function(error, stdout, stderr) {
+		if (error !== null) {
+			console.log('exec error: ' + error);
+		} else {
+			console.log('printing /home/pi/pos/prtsrc/brother/' + file);
+		}
+	});
+	console.log('lp -d ' + printer + ' /home/pi/pos/prtsrc/brother/' + file);
 };
 // =======================================
 // POST Route 
@@ -38,7 +38,7 @@ router.post('/:item/:times', function(req, res, next) {
 		// Bratwurst
 		case "bratwurst":
 			for (var i = 1; i <= req.params['times']; i++) {
-				printLabel('bratwurst.zpl');
+				printLabel('bratwurst.pdf');
 				result += 'print ' + req.params['item'] + '<br>';
 			}
 			break;
@@ -46,7 +46,19 @@ router.post('/:item/:times', function(req, res, next) {
 		// Kloepfer
 		case "kloepfer":
 			for (var i = 1; i <= req.params['times']; i++) {
-				printLabel('kloepfer.zpl');
+				printLabel('kloepfer.pdf');
+				result += 'print ' + req.params['item'] + '<br>';
+			}
+			break;
+		case "steak":
+			for (var i = 1; i <= req.params['times']; i++) {
+				printLabel('steakmitbrot.pdf');
+				result += 'print ' + req.params['item'] + '<br>';
+			}
+			break;
+		case "steapommes":
+			for (var i = 1; i <= req.params['times']; i++) {
+				printLabel('steakmitpommes.pdf');
 				result += 'print ' + req.params['item'] + '<br>';
 			}
 			break;
